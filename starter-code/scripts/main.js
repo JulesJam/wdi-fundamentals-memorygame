01e5fb90c852c1c7c2b53b891d305365a6af14b7
Julian Wyatt <julian.wyatt@1xdconsulting.co.uk>
2016-06-15 20:10:45 +0100

    card game cretaed

--- starter-code/scripts/main.js
+++ starter-code/scripts/main.js
@@ -1,30 +1,120 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+cards = ['queen','queen','king','king'];
+cardsInPlay = [];
+var score = 0;
+
 
 var gameBoard = document.getElementById ('game-board');
 
+function on_load(score){
+ 	
+	var playAgain = document.getElementById('button');
+	playAgain.addEventListener('click',clearCard);
+	updateScore(score);
 
-var createBoard = function(numberOfCards){
-	for (i=0; i <= numberOfCards; i+=1){
-	var createCardDiv =	document.createElement('div');
-	createCardDiv.className = "board";
-	gameBoard.appendChild(createCardDiv);
+
 	}
 
+function updateScore (score){
+
+	var scoreDisplay = document.getElementById('score');
+	
+	scoreDisplay.innerHTML = 'Score '+ score;
+
+}
 
 
-console.log (i);
-console.log (numberOfCards);
+var createBoard = function(numberOfCards){
+	
+	for (i=0; i <numberOfCards; i+=1){
+	var createCardDiv =	document.createElement('div');
+	createCardDiv.className = "card";
+	createCardDiv.setAttribute('data-card',cards[i]);
+	createCardDiv.addEventListener('click', isTwoCards);
+	createCardDiv.innerHTML = '<img src="images/Back.png" alt="Back" />';
+	gameBoard.appendChild(createCardDiv);
+	}
+	on_load(score);
+	};
+
+var clearCard = function(){
+	
+	 var cardClass =document.getElementsByClassName('card');
+	for (i=0; i < cards.length; i+=1){
+	 cardClass[i].innerHTML = '<img src="images/Back.png" alt="Back" />';
+	}
 };
 
 
-createBoard(20);
-/*if(cardTwo === cardFour){
+function isMatch () {
 
-	alert('You found a match');
+	if(cardsInPlay[0] === cardsInPlay[1])
+		{
+		alert('You found a match');
+		score=score+1;
+		updateScore(score);
+		
+		}
+	else {
+		alert('Sorry Try Again');
+		
+		
+		}
+
+	}
+
+
+	
+
+
+ 
+function isTwoCards(clickCount) {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  
+  if (cardsInPlay.length === 0) {
+
+ 	clearCard()
+
+
+  }	
+
+  var cardType = this.getAttribute('data-card');
+ 
+ 	 	if (cardType==='king'){
+	this.innerHTML = '<img src="images/KDiamonds 12.png" alt="King of Diamonds" title ="king"/>';}
+	else if (cardType==='queen')
+	{
+		this.innerHTML = '<img src="images/QDiamonds 12.png" alt="Queen of Diamonds" title = "queen" />';
+	}
+
+
+  cardsInPlay.push(cardType);
+ 
+  // if you have two cards in play check for a match
+
+
+
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+
+  }
+
+ 
+
 }
-else {
-	alert('Sorry Try Again')
-}*/
+
+
+
+
+createBoard(4);
+
+
