d09c73ef73840713ebdfec3c5cd083aaed3b235c
Julian Wyatt <julian.wyatt@1xdconsulting.co.uk>
2016-06-15 16:02:38 +0100

    added JS to create divs

--- starter-code/scripts/main.js
+++ starter-code/scripts/main.js
@@ -1,11 +1,30 @@
 var cardOne = 'queen';
-var cardTwo = 'king';
+var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
-if(cardTwo === cardFour){
+
+var gameBoard = document.getElementById ('game-board');
+
+
+var createBoard = function(numberOfCards){
+	for (i=0; i <= numberOfCards; i+=1){
+	var createCardDiv =	document.createElement('div');
+	createCardDiv.className = "board";
+	gameBoard.appendChild(createCardDiv);
+	}
+
+
+
+console.log (i);
+console.log (numberOfCards);
+};
+
+
+createBoard(20);
+/*if(cardTwo === cardFour){
 
 	alert('You found a match');
 }
 else {
 	alert('Sorry Try Again')
-}
+}*/
