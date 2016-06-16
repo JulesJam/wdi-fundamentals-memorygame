0cc27c6674a07fb40054b876d9fa01e46d0922ce
Julian Wyatt <julian.wyatt@1xdconsulting.co.uk>
2016-06-16 18:04:50 +0200

    updated to reset score on play again but cannot get game to end after number of clicks

--- starter-code/scripts/main.js
+++ starter-code/scripts/main.js
@@ -2,6 +2,8 @@ cardMaster = ['queen','king'];
 cards = [];
 cardsInPlay = [];
 var score = 0;
+var numberOfClicks = 0;
+var numberOfCards = 6;
 
 
 var gameBoard = document.getElementById ('game-board');
@@ -9,7 +11,7 @@ var gameBoard = document.getElementById ('game-board');
 function on_load(score){
  	
 	var playAgain = document.getElementById('button');
-	playAgain.addEventListener('click',clearCard);
+	playAgain.addEventListener('click',reStart);
 	updateScore(score);
 
 
@@ -21,10 +23,12 @@ function updateScore (score){
 	
 	scoreDisplay.innerHTML = 'Score '+ score;
 
+
 }
 
-function generateRandomSequence (numberOfCards){
-	alert('Shuffling');
+function generateRandomSequence (numberOfCards, numberOfClicks){
+	 if (numberOfClicks >0){
+	 	alert('Shuffling');}
 	cards = [];
 	for(i=0; i <numberOfCards; i+=1){
 		var randomPick = Math.floor((Math.random() * 2) +0);
@@ -47,9 +51,7 @@ function setboardWidth(numberOfCards){
 	var requiredWidthStr = ''+requiredWidth+'px';
 	boardWidth.style.width=requiredWidthStr;
 	console.log(requiredWidthStr);	
-	var width = window.innerWidth
-	|| document.documentElement.clientWidth
-	|| document.body.clientWidth;
+	var width = window.innerWidth|| document.documentElement.clientWidth|| document.body.clientWidth;
 	var reqdMargin = (width - requiredWidth)/2;
 	var reqdMarginStr ='5px '+reqdMargin+'px';
 	boardWidth.style.margin=reqdMarginStr;
@@ -64,7 +66,7 @@ function setboardHeight(numberOfCards){
 	console.log(requiredHeightStr);
 }
 
-var createBoard = function(numberOfCards){
+var createBoard = function(numberOfCards, numberOfClicks){
 	setboardWidth(numberOfCards);
 	if (numberOfCards>6){
 	setboardHeight(numberOfCards);
@@ -85,14 +87,37 @@ var createBoard = function(numberOfCards){
 	
 	};
 
-	var clearCard = function(){
-	generateRandomSequence();
+var clearCard = function(){
+
 	 var cardClass =document.getElementsByClassName('card');
 	for (i=0; i < cards.length; i+=1){
 	 cardClass[i].innerHTML = '<img src="images/Back.png" alt="Back" />';
 	}
 	
-};
+	};
+
+
+var reStart = function(score){
+
+	alert('new game')
+	 var cardClass =document.getElementsByClassName('card');
+	for (i=0; i < cards.length; i+=1){
+	 cardClass[i].innerHTML = '<img src="images/Back.png" alt="Back" />';
+	}
+
+	var score=0;
+	updateScore(score);
+	
+	};
+
+function endOfGame(numberOfClicks, numberOfCards){
+		var endOfPlay = ((numberOfCards*2)-2);
+		
+   		if(numberOfClicks > 6){
+   
+    	alert('Game Over');}
+
+}
 
 
 function isMatch () {
@@ -102,33 +127,43 @@ function isMatch () {
 		alert('You found a match');
 		score=score+1;
 		updateScore(score);
+		setTimeout(clearCard(),10000);
 		
 		}
 	else {
 		alert('Sorry Try Again');
-		
-		
+		setTimeout(clearCard(),10000);
 		}
-
+		
+		
 	}
+
+		
+
+	
 
 
 	
 
 
  
-function isTwoCards(clickCount) {
+function isTwoCards(clickCount, numberOfClicks, numberOfCards) {
 
   // add card to array of cards in play
   // 'this' hasn't been covered in this prework, but
   // for now, just know it gives you access to the card the user clicked on
   
-  if (cardsInPlay.length === 0) {
+ 
+ console.log (numberOfClicks);
 
- 	clearCard()
+ numberOfClicks=numberOfClicks+5;
 
 
-  }	
+ if(numberOfClicks>6){
+ 	alert('end');
+ }
+		
+   		
 
   var cardType = this.getAttribute('data-card');
  
@@ -149,11 +184,12 @@ function isTwoCards(clickCount) {
   if (cardsInPlay.length === 2) {
 
     // pass the cardsInPlay as an argument to isMatch function
-    isMatch(cardsInPlay);
+    isMatch(cardsInPlay, numberOfClicks, numberOfCards);
 
 
     // clear cards in play array for next try
     cardsInPlay = [];
+    
 
 
   }
@@ -165,6 +201,6 @@ function isTwoCards(clickCount) {
 
 
 
-createBoard(6);
+createBoard(numberOfCards);
 
 
